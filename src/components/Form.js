63085d453b53dfbec27334e5dda252b2c344545b
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-12-17 20:54:07 +0300

    add buttons and css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-const Form = ({ input, setInput, todos, setTodos }) => {
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) =>
+      todo.id === id ? { title, id, completed } : todo
+    );
+    setTodos(newTodo);
+    setEditTodo("");
+  };
+
+  useEffect(() => {
+    if (editTodo) {
+      setInput(editTodo.title);
+    } else {
+      setInput("");
+    }
+  }, [setInput, editTodo]);
+
   const onInputChange = (event) => {
     setInput(event.target.value);
   };
 
   const onFormSubmit = (event) => {
-    event.preventDefault();
-    setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
-    setInput("");
+    event.preventDefault(); // to prevent page refresh
+    if (!editTodo) {
+      setTodos([...todos, { id: uuidv4(), title: input, completed: false }]);
+      setInput("");
+    } else {
+      updateTodo(input, editTodo.id, editTodo.completed);
+    }
   };
 
   return (
@@ -23,7 +43,7 @@ const Form = ({ input, setInput, todos, setTodos }) => {
         onChange={onInputChange}
       />
       <button className="button-add" type="submit">
-        Add
+        {editTodo ? "OK" : "Add"}
       </button>
     </form>
   );
